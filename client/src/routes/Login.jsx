07062c3016f3a21f054ae0ab85c095abfce7a1f7
CoderSid063 <sudhansupanda063@gmail.com>
2024-05-18 16:48:23 +0530

    feat : add routes

---
+++ client/src/routes/Login.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const user = "";
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    user && navigate("/dashboard");
+  }, [user]);
+
+  return <div>Login</div>;
+};
+
+export default Login;
